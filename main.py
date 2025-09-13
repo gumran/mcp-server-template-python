4f399b52f5681e817b412de7aa4c382c4a9af05a
gumran <a.r.gumran@gmail.com>
2025-09-13 18:43:23 +0200

    tryhard

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ import os
 mcp = FastMCP("Echo Server", port=3000, stateless_http=True, debug=True)
 
 @mcp.tool(
-    title="Tool to Sample from LLM",
+    title="Repeated sampling Tool",
     description="Call another LLM and return its response.",
 )
-def sample_from_llm(query: str = Field(description="The user's query")) -> str:
+def get_response_from_llm(query: str = Field(description="The user's query")) -> str:
     client = Mistral(api_key=os.environ["MISTRAL_API_KEY"])
     model = "mistral-large-latest"
 
@@ -27,10 +27,10 @@ def sample_from_llm(query: str = Field(description="The user's query")) -> str:
     return response.choices[0].message.content
 
 @mcp.tool(
-    title="Answer Review Tool",
+    title="Sequential refinement Tool",
     description="Get a review of an answer to a user's query. Expects the user's original query and the LLM's latest response to the query.",
 )
-def answer_review(query: str = Field(description="The user's query"),
+def get_answer_review(query: str = Field(description="The user's query"),
                            response: str = Field(description="The LLM's latest response")) -> str:
     client = Mistral(api_key=os.environ["MISTRAL_API_KEY"])
     model = "mistral-large-latest"
@@ -47,6 +47,33 @@ def answer_review(query: str = Field(description="The user's query"),
         ],
     )
     return review.choices[0].message.content
+
+@mcp.tool(
+    title="Selecting the best answer Tool",
+    description="Get the index of the best of multiple LLM responses according to an external judge. Expects the user's original query and multiple LLM responses indexed from 1."
+)
+def get_best_answer(query: str = Field(description="The user's query"),
+                           responses: str = Field(description="Multiple LLM responses indexed from 1")) -> str:
+    client = Mistral(api_key=os.environ["MISTRAL_API_KEY"])
+    model = "mistral-large-latest"
+
+    content = f"""You are provided a user's query and multiple LLM responses to that query. Your job is to select the best response among them. If there is a clear best response, return the index of that response. If multiple responses are equally good, return the first one among them. Only return the index and nothing else.
+
+    User's query:
+    {query}
+
+    LLM responses:
+    {responses}
+    """
+    response = client.chat.complete(
+        model=model,
+        messages=[
+            {"role": "user", "content": content},
+        ],
+    )
+    return response.choices[0].message.content
+
+
 
 # @mcp.tool(
 #     title="MCTS Tool",
